48aa30f87df4787d4076dfb60d4ae801e85cbbbb
Koushik <programmer.koushik@gmail.com>
2024-08-19 22:42:50 +0530

    added logout button

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 // Dashboard RootLayout
 
 import DashboardLeftBar from "@/components/layout/DashboardLeftBar";
+import LogoutButton from "@/components/user/LogoutButton";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -14,7 +15,12 @@ export default function DashboardRootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex mx-4 h-screen">
+    <div className="flex mx-4 h-screen mt-8">
+      {/* User */}
+      <div className="absolute top-3 right-4">
+        <LogoutButton />
+      </div>
+
       {/* Left bar */}
       <div>
         <DashboardLeftBar />
