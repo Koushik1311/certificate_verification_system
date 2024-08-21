5656d76beb2a780c378cbc23f15aeb1b340ee06b
Koushik <programmer.koushik@gmail.com>
2024-08-21 14:46:13 +0530

    if there is refresh token then show dashboard otherwise show log in

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,8 +1,13 @@
 import Logo from "../global/Logo";
 import { NavLinks } from "@/constants/nav-links";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
 export default function NavBar() {
+  const cookieStore = cookies();
+
+  const hasCookie = cookieStore.has("refreshToken");
+
   return (
     <div className="flex items-center justify-between mt-3">
       {/* Logo */}
@@ -23,12 +28,21 @@ export default function NavBar() {
       </ul>
       <div>
         {/* Dashboard */}
-        <Link
-          href="/dashboard/upload"
-          className="border border-blue-400 bg-blue-300 hover:border-blue-500 hover:bg-blue-400 transition duration-300 text-white rounded-lg px-3 py-1"
-        >
-          Dashboard
-        </Link>
+        {hasCookie ? (
+          <Link
+            href="/dashboard/upload"
+            className="border border-blue-400 bg-blue-300 hover:border-blue-500 hover:bg-blue-400 transition duration-300 text-white rounded-lg px-3 py-1"
+          >
+            Dashboard
+          </Link>
+        ) : (
+          <Link
+            href="/login"
+            className="px-6 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-400 transition-colors"
+          >
+            Log in
+          </Link>
+        )}
       </div>
     </div>
   );
